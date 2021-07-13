49256384313ec8ea98236702d4210ad7e6cd87a4
muhammet solmaz <msolmaz83@gmail.com>
2021-07-13 19:22:05 +0200

    search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    state = {
+        searchQuery :" "
+    }
+
+
+     handleFormSubmit=(event) => {
+         event.preventDefault();
+     }
     render() {
 
         return  (
-            <h1>Search Bar</h1>
+            <form onSubmit={this.handleFormSubmit}>
+                <div className="form-row mb-5">
+                    <div className="col-12">
+                    <input onChange={(event) => this.setState({searchQery: event.target.value})}
+                    type="text" className="form-control" 
+                    placeholder="search"
+                    value={this.state.searchQuery}></input>
+                   
+                    </div>
+                </div>
+            </form>
         )
 
     }
